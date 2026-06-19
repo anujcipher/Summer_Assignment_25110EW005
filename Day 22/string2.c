c72f161cb73a107e005f8f6a336ab00a7a205417
anujcipher <ap1327213@gmail.com>
2026-06-19 16:39:24 +0530

    Day 22

---
+++ Day 22/string2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i = 0, words = 1;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    // Count spaces
+    while (str[i] != '\0') {
+        if (str[i] == ' ') {
+            words++;
+        }
+        i++;
+    }
+
+    printf("Number of words = %d\n", words);
+
+    return 0;
+}
